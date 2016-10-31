bbeaf7f2119e4cea42a6c072fb8b13d1b7a10ad1
Jesse Kleve <jesse.kleve@gmail.com>
2016-10-31 17:40:59 -0500

    added comments to settings

--- genetic_algorithm/ga_settings.py
+++ genetic_algorithm/ga_settings.py
@@ -1,13 +1,15 @@
 settings = {
-    'population_size': 20,
+    'population_size': 20, # number of organisms
     'number_of_dimensions': 2,
-    'bounds': [
+    'bounds': [ # this must have 1 pair per dimension
         (-10,10),
         (-10,10)
     ],
     'selection_multiplier': 0.85,
     'selection_cutoff': 0.5,
-    'step_through': False,
-    'plot': True,
-    'debug': True
+    'mutation_rate': 0.02, # how often a mutation happens (between 0 and 1)
+    'mutation_amount': 0.4, # how large of a mutation is allowed (between 0 and 1)
+    'step_through': False, # TODO not implemented. whether it should pause after each iteration
+    'plot': True, # whether we should plot each iteration
+    'debug': True # whether we should print each iteration
 }
