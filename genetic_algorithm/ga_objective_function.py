351e189ba8a4811515fbb5b0615d2fc4e47e9889
Jesse Kleve <jesse.kleve@gmail.com>
2016-10-31 13:47:29 -0500

    added objective function file. added fitness function to organism

---
+++ genetic_algorithm/ga_objective_function.py
@@ -0,0 +1,4 @@
+def objective_function(params):
+    a = params[0]
+    b = params[1]
+    return a + b
--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -1,19 +1,25 @@
 import random
+import sys
 
 from ga_settings import settings
+from ga_objective_function import objective_function as func
 
 class Organism:
     id = 0
-    f = 0
     num_dims = 0
     pos = []
+    f = 0
+    func = None
 
-    def __init__(self, id, num_dims, pos):
+    def __init__(self, id, num_dims, pos, func):
         self.id = id
         self.num_dims = num_dims
         self.pos = pos
-        print(id)
-        print(pos)
+        self.func = func
+        self.f = self.fitness()
+
+    def fitness(self):
+        return self.func(self.pos)
 
 class GA:
     num_dims = 0
@@ -35,11 +41,21 @@ class GA:
     def init_population(self):
         for i in range(0, self.population_size):
             pos = self.get_rand_pos()
-            self.population.append(Organism(i+1, self.num_dims, pos))
+            self.population.append(Organism(i+1, self.num_dims, pos, func))
 
     def get_rand_pos(self):
         b = self.bounds
         return [random.randint(b[i][0], b[i][1]) for i in range(0, self.num_dims)]
 
+    def next_generation(self):
+        print("to implement ...")
+
 if __name__ == "__main__":
     ga = GA()
+    sys.exit()
+
+    params = []
+    params.append(2)
+    params.append(3)
+    print(func(params))
+    sys.exit()
