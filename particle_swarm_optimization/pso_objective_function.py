de003c97566b18dc7e532b5221495736c1ca9d29
Jacob Neyens <jneyens@iastate.edu>
2016-11-16 18:45:24 -0600

    started pso

---
+++ particle_swarm_optimization/pso_objective_function.py
@@ -0,0 +1,8 @@
+#from math import sqrt, cos
+from numpy import sqrt, cos
+
+def objective_function(params):
+    x1 = params[0]
+    x2 = params[1]
+    #return (x1**2 + x2**2)
+    return ( 1.0 + 1.0/4000.0*(x1**2 + x2**2) - ( cos(x1/sqrt(1.0))*cos(x2/sqrt(2.0)) ) )
