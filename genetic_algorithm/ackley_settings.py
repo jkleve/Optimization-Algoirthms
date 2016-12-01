249f8cb8ac0d2e11c932f019410e767c32f25525
Jesse Kleve <jesse.kleve@gmail.com>
2016-12-01 12:18:26 -0600

    changed ackley function to objective function. added response surfaces test

--- genetic_algorithm/ackley_settings.py
+++ genetic_algorithm/ackley_settings.py
@@ -8,11 +8,13 @@ settings = {
     'selection_cutoff': 0.8,
     'mutation_rate': 0.1, # how often a mutation happens (between 0 and 1)
     'max_mutation_amount': 0.4, # how large of a mutation is allowed (between 0 and 1)
-    'num_generations': 100,
+    'num_iterations': 100,
     'time_delay': 0.0,
     'step_through': False, # whether it should pause after each iteration
     'plot': False, # whether we should plot each iteration
     'print_actions': False, # whether we should print when a method does an action
     'print_iterations': False, # whether we should print after each iteration
-    'time': False # whether we should output timing information
+    'time': False, # whether we should output timing information
+
+    'time_as_float': True
 }
--- genetic_algorithm/oa_tests.py
+++ genetic_algorithm/oa_tests.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from statistics import median
 
 from genetic_algorithm import GA as OptimizationAlgorithm
-import ga_settings
-import ga_objective_function
+from ackley_settings import settings
+from ackley_function import objective_function
 
 def num_parts_vs_time(o_algorithm, num_parts):
     tests = {}
 
     for test in num_parts:
-        ga_settings.settings['population_size'] = test
+        settings['population_size'] = test
 
-        algorithm = o_algorithm(ga_settings.settings, ga_objective_function.objective_function)
+        algorithm = o_algorithm(settings, objective_function)
 
         algorithm.start_timer()
         algorithm.run()
@@ -30,11 +33,11 @@ def func_val_vs_iterations(o_algorithm, num_parts):
     for test in num_parts:
         f = []
 
-        ga_settings.settings['population_size'] = test
+        settings.settings['population_size'] = test
 
-        algorithm = o_algorithm(ga_settings.settings, ga_objective_function.objective_function)
+        algorithm = o_algorithm(settings, objective_function)
 
-        while ga_settings.settings['num_iterations'] > algorithm.num_generations:
+        while settings['num_iterations'] > algorithm.num_generations:
             f.append(algorithm.get_best_f())
             algorithm.do_loop()
 
@@ -50,7 +53,68 @@ def func_val_vs_iterations(o_algorithm, num_parts):
 
 if __name__ == "__main__":
 
-    num_particles = [50, 100, 500]# + range(1000, 10000, 1000)
+    #num_particles = [50, 100, 500]# + range(1000, 10000, 1000)
     #tests = num_parts_vs_time(OptimizationAlgorithm, num_particles)
 
-    func_val_vs_iterations(OptimizationAlgorithm, num_particles)
+    #func_val_vs_iterations(OptimizationAlgorithm, num_particles)
+
+    tests = {}
+
+
+    # selection cutoff [0.1, 0.9]
+    # mutation rate    [0.05, 0.8]
+    # number of runs 50 w/ 50 particles
+    settings['population_size'] = 50
+    num_tests_per = 50
+    sc_start = 0.1
+    sc_end   = 0.9
+    sc_step  = 0.1
+    mt_start = 0.1
+    mt_end   = 0.8
+    mt_step  = 0.1
+    num_tests = ( (sc_end-sc_start)/sc_step + 1 )*( (mt_end-mt_start)/mt_step + 1 )
+    X = np.ones(shape=(num_tests,6))
+    y = np.zeros(shape=(num_tests,1))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in np.arange(sc_start, sc_end+sc_step, sc_step):
+        for j in np.arange(mt_start, mt_end+mt_step, mt_step):
+            settings['selection_cutoff'] = i
+            settings['mutation_rate'] = j
+
+            values = []
+            test_name = str(i) + ',' + str(j)
+            print("Running test %s" % test_name)
+
+            for k in range(0,num_tests_per):
+                algorithm = OptimizationAlgorithm(settings, objective_function)
+                algorithm.run()
+
+                values.append(algorithm.get_best_x().get_fitness())
+                X[k][1] = i
+                X[k][2] = j
+                X[k][3] = i**i
+                X[k][4] = i**j
+                X[k][5] = j**j
+                y[k] = algorithm.get_best_x().get_fitness()
+
+            #avg = sum(values)/len(values)
+            avg = median(values)
+            tests[test_name] = avg
+            ax.scatter(i,j,avg)
+
+    XtX = np.matmul(X.transpose(), X)
+    XtXinv = np.linalg.inv(XtX)
+    XtXinvXt = np.matmul(XtXinv, X.transpose())
+
+    b = np.matmul(XtXinvXt, y)
+
+    print(b)
+
+    print(tests)
+    ax.set_xlabel('Selection Cutoff')
+    ax.set_ylabel('Mutation Rate')
+    ax.set_zlabel('Averaged Objective Function Value')
+    #plt.show()
