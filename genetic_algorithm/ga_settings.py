0c8bc986ab1ebfdacc3ef469160b8383713e01c8
Jesse Kleve <jesse.kleve@gmail.com>
2016-12-06 14:30:40 -0600

    removed png's and ackley from ga.

--- genetic_algorithm/ga_settings.py
+++ genetic_algorithm/ga_settings.py
@@ -5,10 +5,10 @@ settings = {
         (-10,10),
         (-10,10)
     ],
-    'selection_cutoff': 0.8,
-    'mutation_rate': 0.1, # how often a mutation happens (between 0 and 1)
-    'max_mutation_amount': 0.4, # how large of a mutation is allowed (between 0 and 1)
-    'num_iterations': 100,
+    'selection_cutoff': 0.3,
+    'mutation_rate': 0.9, # how often a mutation happens (between 0 and 1)
+    'max_mutation_amount': 1.0, # how large of a mutation is allowed (between 0 and 1)
+    'num_iterations': 200,
     'time_delay': 0.0,
     'step_through': False, # whether it should pause after each iteration
     'plot': False, # whether we should plot each iteration
