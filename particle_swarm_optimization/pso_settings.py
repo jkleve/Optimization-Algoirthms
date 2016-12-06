22d665b4b6399f626ea92ac95ec1c52708881c26
Jacob Neyens <jneyens@iastate.edu>
2016-12-06 09:55:22 -0600

    stuff

--- particle_swarm_optimization/pso_settings.py
+++ particle_swarm_optimization/pso_settings.py
@@ -1,5 +1,5 @@
 settings = {
-    'population_size': 100, # number of organisms
+    'population_size': 500, # number of organisms
     'number_of_dimensions': 2,  # I don't think we can do any dimensions greater than 2
     'bounds': [ # this must have 1 pair per dimension
         (-10,10),
@@ -8,8 +8,8 @@ settings = {
     'num_iterations': 100,
     'time_delay': 0.0,
     ##### Leave these between 0 and 4 #####
-    'cp': 0.5,          # Weight that has the particles tend to go to local known minimum
-    'cg': 0.5,          # Weight that has the particles tend to go to global minimum
+    'cp': 0.2,          # Weight that has the particles tend to go to local known minimum
+    'cg': 0.8,          # Weight that has the particles tend to go to global minimum
     'weight': 0.5,      # this is only used when velocity_type is inertia... haven't really seen much change though when altering this
     'velocity_type': 'inertia', # normal, inertia, or constriction
     'step_through': False, # whether it should pause after each iteration
