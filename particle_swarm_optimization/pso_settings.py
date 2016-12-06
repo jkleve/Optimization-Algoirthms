f81f0ed787183df2515bc5bf254262db4300bfa3
Jacob Neyens <jneyens@iastate.edu>
2016-12-06 10:25:38 -0600

    asdf

--- particle_swarm_optimization/pso_settings.py
+++ particle_swarm_optimization/pso_settings.py
@@ -1,13 +1,14 @@
 settings = {
     'population_size': 500, # number of organisms
-    'number_of_dimensions': 2,  # I don't think we can do any dimensions greater than 2
+    'number_of_dimensions': 2,  # if you change the dimensions, you need to change bounds and change plot to false
     'bounds': [ # this must have 1 pair per dimension
         (-10,10),
         (-10,10)
     ],
     'num_iterations': 100,
     'time_delay': 0.0,
-    ##### Leave these between 0 and 4 #####
+    ##### Leave these between 0 and 1 #####
+
     'cp': 0.2,          # Weight that has the particles tend to go to local known minimum
     'cg': 0.8,          # Weight that has the particles tend to go to global minimum
     'weight': 0.5,      # this is only used when velocity_type is inertia... haven't really seen much change though when altering this
